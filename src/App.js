7440089b2cfdc50ed462ab8fe7f6f4300d570caa
woody <@>
2020-11-21 00:32:11 +0900

    #3.2 state and life cycle

--- src/App.js
+++ src/App.js
@@ -1,69 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.5,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 3.4,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.0,
-  },
-];
-
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h1>{rating}/5.0</h1>
-      <img src={image} alt={name}></img>
-    </div>
-  );
+function doSomething() {
+  console.log("hello button");
 }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
+class App extends React.Component {
 
+  // constructor
+  constructor(props){
+    super(props);
+    console.log("construct");
+  }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((item) => {
-        return <Food name={item.name} image={item.image} key={item.id} rating={item.rating} />;
-      })}
-    </div>
-  );
+  // mount
+  componentDidMount(){
+    console.log("component rendered");
+  }
+
+  // update
+  componentDidUpdate(){
+    console.log("component updated");
+  }
+
+  // unmount
+  componentWillUnmount(){
+    console.log("component will closed");
+  }
+
+  state = {
+    count: 0,
+  };
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+
+  add = () => {
+    console.log("add");
+    this.setState(current => ({count:current.count+1}));
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState(item => ({count:item.count-1}));
+  };
+
+  
 }
 
 export default App;
